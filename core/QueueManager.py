39ca63d35c8538a27f9f56487f833ec865b47e00
kumar <akashkumarva@gmail.com>
2023-10-29 15:44:41 -0400

    Testing

--- core/QueueManager.py
+++ core/QueueManager.py
@@ -3,13 +3,13 @@ from discord_lambda import Components
 
 
 join_queue_custom_id = "join_queue"
-leave_queue_custom_id = "join_queue"
-start_queue_custom_id = "join_queue"
+leave_queue_custom_id = "leave_queue"
+start_queue_custom_id = "start_queue"
 
 def create_queue_resources() -> (Embedding, Components):
     embed = Embedding("Underworld 8s", "Queue size: 0", color=0x880808)
     component = Components()
     component.add_button("Join queue", join_queue_custom_id, False, 1)
-    component.add_button("Leave queue", leave_queue_custom_id, False, 4)
-    component.add_button("Start queue", start_queue_custom_id, True, 3)
+    #component.add_button("Leave queue", leave_queue_custom_id, False, 4)
+    #component.add_button("Start queue", start_queue_custom_id, True, 3)
     return (embed, component)
