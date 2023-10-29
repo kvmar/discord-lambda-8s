cb93b857e937e35ec4f0e552e23bf303a2d3afec
kumar <akashkumarva@gmail.com>
2023-10-29 19:59:27 -0400

    Init queue dao

--- dao/QueueDao.py
+++ dao/QueueDao.py
@@ -80,4 +80,4 @@ class QueueDao:
       team_2.add(user)
 
 
-    return QueueRecord(response["guild_id"], response["queue_id"], team_1, team_2, queue, response['version'])
+    return QueueRecord(guild_id=response["guild_id"], queue_id=response["queue_id"], team_1=team_1, team_2=team_2, queue=queue, version=response['version'], message_id=response['message_id'])
