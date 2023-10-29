f1acc0d860041822305df0b004fa85fc1cc0e41f
kumar <akashkumarva@gmail.com>
2023-10-29 13:33:04 -0400

    init queue cmd

---
+++ commands/queue.py
@@ -0,0 +1,16 @@
+from core import QueueManager
+from discord_lambda import Embedding, CommandRegistry, Interaction, CommandArg
+
+def queue(inter: Interaction, command: str = "queue") -> None:
+  embed = None
+  if command == "queue":
+    embed = QueueManager.create_queue()
+
+  inter.send_response(embeds=[embed])
+
+
+def setup(registry: CommandRegistry):
+  registry.register_cmd(func=queue, name="help", desc="Adds 8s queue to discord.", options=[])
+
+
+
