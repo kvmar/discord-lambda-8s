66bc3812180ffec7c388de1404d4a1415daceb93
kumar <akashkumarva@gmail.com>
2023-10-29 15:47:41 -0400

    Testing

--- core/QueueManager.py
+++ core/QueueManager.py
@@ -10,6 +10,6 @@ def create_queue_resources() -> (Embedding, Components):
     embed = Embedding("Underworld 8s", "Queue size: 0", color=0x880808)
     component = Components()
     component.add_button("Join queue", join_queue_custom_id, False, 1)
-    #component.add_button("Leave queue", leave_queue_custom_id, False, 4)
-    #component.add_button("Start queue", start_queue_custom_id, True, 3)
+    component.add_button("Leave queue", leave_queue_custom_id, False, 4)
+    component.add_button("Start queue", start_queue_custom_id, True, 3)
     return (embed, component)
