c5e235623af3294b171d423655d164ad998628d6
kumar <akashkumarva@gmail.com>
2023-10-29 19:36:29 -0400

    Init queue dao

--- core/ButtonManager.py
+++ core/ButtonManager.py
@@ -4,8 +4,9 @@ from discord_lambda import Interaction
 
 def button_flow_tree(interaction: Interaction):
   if interaction.custom_id == QueueManager.join_queue_custom_id:
-    join_queue_button(interaction.guild_id)
+    join_queue_button(interaction.guild_id, interaction)
 
-def join_queue_button(guild_id: str):
+def join_queue_button(guild_id: str, inter: Interaction):
   print("Join queue button clicked")
-  QueueManager.update_queue_resources(guild_id)
+  (embed, component) = QueueManager.update_queue_resources(guild_id)
+  inter.edit_response(components=[component], embeds=[embed], ephemeral=False)
