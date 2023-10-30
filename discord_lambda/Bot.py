84b2ae30a306e52a41f4106ae93b7eb4c05b0c73
kumar <akashkumarva@gmail.com>
2023-10-29 22:21:16 -0400

    Init queue dao

--- discord_lambda/Bot.py
+++ discord_lambda/Bot.py
@@ -3,8 +3,7 @@ import datetime
 import os
 
 import discord
-from discord import Message, Guild
-from discord.state import Channel
+from discord import Message, Guild, TextChannel
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -28,14 +27,13 @@ def edit_message(message_id: str, channel_id: str, guild_id: str):
     loop.run_until_complete(close_bot())
 
 
-
 async def fetch_guild(guild_id: int):
     return await bot.fetch_guild(int(guild_id))
 
 async def fetch_channel(guild: Guild, channel_id: int):
     return await guild.fetch_channel(int(channel_id))
 
-async def fetch_message(channel: Channel, message_id: int):
+async def fetch_message(channel: TextChannel, message_id: int):
     return await channel.fetch_message(int(message_id))
 
 async def edit_message(msg: Message):
