4e634968bfebbea5f4277f3d47822343fb2fc747
kumar <akashkumarva@gmail.com>
2023-10-30 16:23:48 -0400

    Fix start match btn bug

--- commands/queue.py
+++ commands/queue.py
@@ -7,7 +7,7 @@ def queue(inter: Interaction, command: str = "queue") -> None:
   if command == "queue":
     (embed, component) = QueueManager.create_queue_resources(inter.guild_id)
 
-  resp = inter.send_response(components=component, embeds=embed, ephemeral=False)
+  resp = inter.send_response(components=[component], embeds=[embed], ephemeral=False)
   print(f'Queue message_id: {resp}')
   QueueManager.update_message_id(inter.guild_id, resp[0], resp[1])
 
