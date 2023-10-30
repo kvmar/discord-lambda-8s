248dc0a7669f8345fe670b09fb67432a95f85a44
kumar <akashkumarva@gmail.com>
2023-10-29 21:48:40 -0400

    Init queue dao

--- discord_lambda/Bot.py
+++ discord_lambda/Bot.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 import discord
@@ -5,11 +6,11 @@ import discord
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
-print(os.environ.values())
-bot.run(os.environ.get('BOT_TOKEN'))
 
 async def edit_message(message_id: str, channel_id: str, guild_id: str):
+    bot.run(os.environ.get('BOT_TOKEN'))
     guild = await bot.fetch_guild(int(guild_id))
     channel = await guild.fetch_channel(int(channel_id))
     msg = await channel.fetch_message(int(message_id))
-    await msg.edit(content="edit")
+    await msg.edit(content="edit" + str(datetime.datetime.now()))
+    await bot.close()
