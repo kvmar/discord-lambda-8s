b81b77d0449853a56a827def2c3c2faea5570306
kumar <akashkumarva@gmail.com>
2023-10-30 18:02:24 -0400

    Add playerpick buttons

--- dao/QueueDao.py
+++ dao/QueueDao.py
@@ -12,7 +12,7 @@ from dao import set_default
 table_name = "QueueTable"
 
 class QueueRecord:
-  def __init__(self, guild_id: str, queue_id: str, team_1: set, team_2: set, queue: set, version: int, expiry: int, message_id: str = None, channel_id: str = None):
+  def __init__(self, guild_id: str, queue_id: str, team_1: list, team_2: list, queue: set, version: int, expiry: int, message_id: str = None, channel_id: str = None):
     self.guild_id = guild_id
     self.queue_id = queue_id
     self.team_1 = team_1
@@ -24,8 +24,8 @@ class QueueRecord:
     self.expiry = expiry
 
   def clear_queue(self):
-    self.team_1 = set()
-    self.team_2 = set()
+    self.team_1 = list()
+    self.team_2 = list()
     self.queue = set()
     self.message_id = None
     self.channel_id = None
@@ -82,13 +82,13 @@ class QueueDao:
     for user in response['queue']:
       queue.add(user)
 
-    team_1 = set()
+    team_1 = list()
     for user in response['team_1']:
-      team_1.add(user)
+      team_1.append(user)
 
-    team_2 = set()
+    team_2 = list()
     for user in response['team_2']:
-      team_2.add(user)
+      team_2.append(user)
 
 
     return QueueRecord(guild_id=response["guild_id"], queue_id=response["queue_id"], expiry=int(response["expiry"]), team_1=team_1, team_2=team_2, queue=queue, version=response["version"], message_id=response["message_id"], channel_id=response["channel_id"])
