578ca4f0484d769943159fb155c0e53132c12b68
kumar <akashkumarva@gmail.com>
2023-10-29 22:16:00 -0400

    Init queue dao

--- core/ButtonManager.py
+++ core/ButtonManager.py
@@ -3,13 +3,13 @@ from discord_lambda import Interaction, Bot
 from dao.QueueDao import QueueDao
 
 queue_dao = QueueDao()
-async def button_flow_tree(interaction: Interaction):
+def button_flow_tree(interaction: Interaction):
   if interaction.custom_id == QueueManager.join_queue_custom_id:
-    await join_queue_button(interaction.guild_id, interaction)
+    join_queue_button(interaction.guild_id, interaction)
 
-async def join_queue_button(guild_id: str, inter: Interaction):
+def join_queue_button(guild_id: str, inter: Interaction):
   print("Join queue button clicked")
   (embed, component) = QueueManager.update_queue_resources(guild_id)
 
   record = queue_dao.get_queue(guild_id=guild_id, queue_id="1")
-  await Bot.edit_message(message_id=record.message_id, channel_id=record.channel_id, guild_id=record.guild_id)
+  Bot.edit_message(message_id=record.message_id, channel_id=record.channel_id, guild_id=record.guild_id)
--- discord_lambda/Bot.py
+++ discord_lambda/Bot.py
@@ -1,16 +1,42 @@
+import asyncio
 import datetime
 import os
 
 import discord
+from discord import Message, Guild
+from discord.state import Channel
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = discord.Client(intents=intents)
 
-async def edit_message(message_id: str, channel_id: str, guild_id: str):
+def edit_message(message_id: str, channel_id: str, guild_id: str):
     bot.run(os.environ.get('BOT_TOKEN'))
-    guild = await bot.fetch_guild(int(guild_id))
-    channel = await guild.fetch_channel(int(channel_id))
-    msg = await channel.fetch_message(int(message_id))
+    loop = asyncio.get_event_loop()
+    guild = loop.run_until_complete(fetch_guild(int(guild_id)))
+
+    loop = asyncio.get_event_loop()
+    channel = loop.run_until_complete(fetch_channel(guild, int(channel_id)))
+
+    loop = asyncio.get_event_loop()
+    msg = loop.run_until_complete(fetch_channel(channel, int(message_id)))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(edit_message(msg))
+    await bot.close()
+
+
+async def fetch_guild(guild_id: int):
+    return await bot.fetch_guild(int(guild_id))
+
+async def fetch_channel(guild: Guild, channel_id: int):
+    return await guild.fetch_channel(int(channel_id))
+
+async def fetch_message(channel: Channel, message_id: int):
+    return await channel.fetch_message(int(message_id))
+
+async def edit_message(msg: Message):
     await msg.edit(content="edit" + str(datetime.datetime.now()))
+
+async def close_bot():
     await bot.close()
