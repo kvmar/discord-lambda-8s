d1f89c2256f394c6d807460acbbdc6f17abcce79
kumar <akashkumarva@gmail.com>
2023-10-31 19:45:38 -0400

    Add additional queue cmd

--- commands/queue.py
+++ commands/queue.py
@@ -1,11 +1,12 @@
 from core import QueueManager
 from discord_lambda import Embedding, CommandRegistry, Interaction, CommandArg
 
-def queue(inter: Interaction, command: str = "queue") -> None:
+def queue(inter: Interaction, queue_id: str) -> None:
   embed = None
   component = None
-  if command == "queue":
-    (embed, component) = QueueManager.create_queue_resources(inter.guild_id)
+  print(f"Creating queue with: {queue_id} in guild_id: {inter.guild_id}")
+
+  (embed, component) = QueueManager.create_queue_resources(inter.guild_id)
 
   resp = inter.send_response(components=[component], embeds=[embed], ephemeral=False)
   print(f'Queue message_id: {resp}')
@@ -13,7 +14,7 @@ def queue(inter: Interaction, command: str = "queue") -> None:
 
 
 def setup(registry: CommandRegistry):
-  registry.register_cmd(func=queue, name="queue", desc="Adds 8s queue to discord", options=[])
+  registry.register_cmd(func=queue, name="queue", desc="Adds 8s queue to discord", options=[CommandArg("queue_name", "Queue name", CommandArg.Types.STRING)])
 
 
 
