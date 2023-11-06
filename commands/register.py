a1eb303687ad2382085b3cd10df24e80b1f700f2
kumar <akashkumarva@gmail.com>
2023-11-06 18:56:31 -0500

    Add registration accessor

--- commands/register.py
+++ commands/register.py
@@ -4,8 +4,8 @@ from venmoapi import VenmoApiAccessor
 
 venmo = VenmoApiAccessor()
 def register(inter: Interaction, venmo_user: str) -> None:
-  venmo.register_user(inter.user_id, inter.guild_id, venmo_user)
-  embed = Embedding("Kali 8s Bot", f"Sent registration fee 8s user {inter.username} with venmo: {venmo_user} :smiley:. Please accept Venmo request to complete registration", color=0x00FF00)
+  payment_id = venmo.register_user(inter.user_id, inter.guild_id, venmo_user)
+  embed = Embedding("Kali 8s Bot", f"Sent registration fee 8s user {inter.username} with venmo: {venmo_user} payment id: {payment_id} :smiley:. Please accept Venmo request to complete registration", color=0x00FF00)
   inter.send_response(embeds=[embed], ephemeral=False)
 
 def setup(registry: CommandRegistry):
