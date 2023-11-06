7875602bd63a8cb7a3f288431b6070ea578e9837
kumar <akashkumarva@gmail.com>
2023-11-06 18:50:42 -0500

    Add registration accessor

--- commands/register.py
+++ commands/register.py
@@ -1,10 +1,12 @@
 from core import QueueManager
 from discord_lambda import Embedding, CommandRegistry, Interaction, CommandArg
+from venmoapi import VenmoApiAccessor
 
+venmo = VenmoApiAccessor()
 def register(inter: Interaction, venmo_user: str) -> None:
-  embed = Embedding("Kali 8s Bot", f"Registered Money 8S user {inter.username} with venmo: {venmo_user} :smiley:", color=0x00FF00)
+  venmo.register_user(inter.user_id, inter.guild_id, venmo_user)
+  embed = Embedding("Kali 8s Bot", f"Sent registration fee 8s user {inter.username} with venmo: {venmo_user} :smiley:. Please accept Venmo request to complete registration", color=0x00FF00)
   inter.send_response(embeds=[embed], ephemeral=False)
-
 
 def setup(registry: CommandRegistry):
   registry.register_cmd(func=register, name="register", desc="Register for money 8s", options=[CommandArg("venmo_user", "Venmo User Name", CommandArg.Types.STRING)])
---
+++ venmoapi/__init__.py
@@ -0,0 +1,61 @@
+""""
+Copyright © Krypton 2019-2023 - https://github.com/kkrypt0nn (https://krypton.ninja)
+Description:
+🐍 A simple template to start to code your own and personalized discord bot in Python programming language.
+
+Version: 6.1.0
+"""
+import os
+
+import requests
+from trueskill import TrueSkill
+from trueskill import Rating
+
+from dao.PlayerBankDao import PlayerBankDao
+from dao.PlayerDao import PlayerDao, PlayerRecord
+
+player_dao = PlayerDao()
+player_bank_dao = PlayerBankDao()
+
+base_api = "https://api.venmo.com/v1/"
+
+class VenmoApiAccessor:
+    def __init__(self) -> None:
+        self.env = os.environ.get('BOT_ENV')
+
+    def register_user(self, player_id: str, guild_id: str, venmo_user: str):
+        player_record = player_dao.get_player(player_id=player_id, guild_id=guild_id)
+        payment_id = self.request_payment(venmo_user, f"Registration for user {player_record.player_name} to Kali 8s")
+        print(payment_id)
+
+    def deposit(self, player_id: str):
+        pass
+
+    def withdraw(self, player_id: str):
+        pass
+
+    def request_payment(self, venmo_user: str, description: str):
+        try:
+            headers = {
+                "Authorization": f"Bearer {os.environ.get('VENMO_TOKEN')}"
+            }
+
+            json = {
+                "note": description,
+                "amount": -1,
+                "username": venmo_user,
+                "audience": "private"
+            }
+
+            response = requests.post(f'{base_api}/payments', json=json, headers=headers)
+            print(f'Got SendResponse: {response.text}')
+            response.raise_for_status()
+            print(f'Convert to JSON SendResponse: {response.json}')
+            return response.json()["data"]["payment"]["id"]
+        except Exception as e:
+            raise Exception(f"Unable to send message: {e}")
+
+
+
+
+
