9058f8dbf1b8cc20262d00364cc21f35fe2f7ab8
kumar <akashkumarva@gmail.com>
2023-11-06 21:43:55 -0500

    Post money 8s results

--- core/QueueManager.py
+++ core/QueueManager.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from core import LeaderboardManager
+from dao.PlayerBankDao import PlayerBankRecord, PlayerBankDao
 from dao.PlayerDao import PlayerDao, PlayerRecord
 from dao.QueueDao import QueueDao, QueueRecord
 from discord_lambda import Embedding, Interaction
@@ -20,6 +21,7 @@ team_2_won_custom_id = "team_2_won"
 
 queue_dao = QueueDao()
 player_dao = PlayerDao()
+player_bank_dao = PlayerBankDao()
 ts = TrueSkillAccessor()
 venmo = VenmoApiAccessor()
 
@@ -44,6 +46,25 @@ def create_queue_resources(guild_id: str, queue_name: str):
 
 def add_player(inter: Interaction, queue_id: str):
     response = queue_dao.get_queue(guild_id=inter.guild_id, queue_id=queue_id)
+
+    if response.money_queue:
+        player_bank_record = player_bank_dao.get_player_bank(player_id=inter.user_id)
+        if player_bank_record is None:
+            embed = Embedding("Kali 8s Bot", f"Register to Money 8s before checking balance using /register :smiley:", color=0x00FF00)
+            inter.send_response(embeds=[embed], ephemeral=False)
+            return
+
+        if not player_bank_record.registration_complete:
+            embed = Embedding("Kali 8s Bot", f"Registration still pending for user {inter.username} with venmo: {player_bank_record.venmo_user}. Please accept venmo request :smiley:", color=0x00FF00)
+            inter.send_response(embeds=[embed], ephemeral=False)
+            return
+
+        if not player_bank_record.credits < 1:
+            embed = Embedding("Kali 8s Bot", f"{inter.username} you need atleast a balance of $1 to join queue :smiley:", color=0x00FF00)
+            inter.send_response(embeds=[embed], ephemeral=False)
+            return
+
+
     curr_time = int(datetime.utcnow().timestamp())
     if curr_time > response.expiry and len(response.team_1) == 0 and len(response.team_2) == 0:
         print("Resetting queue after 10 mins")
