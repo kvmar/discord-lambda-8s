61260578114f4559d425ee4a3c1ce77b4a15dfeb
kumar <akashkumarva@gmail.com>
2023-11-06 22:01:11 -0500

    Fix message sending

--- core/QueueManager.py
+++ core/QueueManager.py
@@ -51,17 +51,17 @@ def add_player(inter: Interaction, queue_id: str):
         player_bank_record = player_bank_dao.get_player_bank(player_id=inter.user_id)
         if player_bank_record is None:
             embed = Embedding("Kali 8s Bot", f"Register to Money 8s before checking balance using /register :smiley:", color=0x00FF00)
-            inter.send_response(embeds=[embed], ephemeral=False)
+            inter.send_message(channel_id=response.channel_id, embeds=[embed], ephemeral=False)
             return
 
         if not player_bank_record.registration_complete:
             embed = Embedding("Kali 8s Bot", f"Registration still pending for user {inter.username} with venmo: {player_bank_record.venmo_user}. Please accept venmo request :smiley:", color=0x00FF00)
-            inter.send_response(embeds=[embed], ephemeral=False)
+            inter.send_response(channel_id=response.channel_id, embeds=[embed], ephemeral=False)
             return
 
         if not player_bank_record.credits < 1:
             embed = Embedding("Kali 8s Bot", f"{inter.username} you need atleast a balance of $1 to join queue :smiley:", color=0x00FF00)
-            inter.send_response(embeds=[embed], ephemeral=False)
+            inter.send_response(channel_id=response.channel_id, embeds=[embed], ephemeral=False)
             return
 
 
