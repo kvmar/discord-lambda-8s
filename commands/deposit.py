c8dbf3811f55f436ef1335e0a053b7d081d810a9
kumar <akashkumarva@gmail.com>
2023-11-06 20:57:55 -0500

    add money 8s commands

--- commands/deposit.py
+++ commands/deposit.py
@@ -15,7 +15,7 @@ def deposit(inter: Interaction) -> None:
     return
 
   if not player_bank_record.registration_complete:
-    embed = Embedding("Kali 8s Bot", f"Registration still pending for user {inter.username} with venmo: {player_bank_record.venmo_user} :smiley:", color=0x00FF00)
+    embed = Embedding("Kali 8s Bot", f"Registration still pending for user {inter.username} with venmo: {player_bank_record.venmo_user}. Please accept Venmo request :smiley:", color=0x00FF00)
     inter.send_response(embeds=[embed], ephemeral=False)
     return
 
