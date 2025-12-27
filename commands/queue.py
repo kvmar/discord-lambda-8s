8d6399534bfdaf5da31a0e6462f5e650bb57a4e3
kumvr <akashkumarva@gmail.com>
2025-12-26 20:30:40 -0500

    fix queue

--- commands/queue.py
+++ commands/queue.py
@@ -3,7 +3,7 @@ from discord_lambda import CommandRegistry, Interaction, CommandArg
 
 def queue(inter: Interaction, queue_name: str = "1") -> None:
   print(f"Creating queue with: {queue_name} in guild_id: {inter.guild_id}")
-  if inter.guild_id != "1123491132765110302":
+  if inter.guild_id != "1123491132765110302" and queue_name != 'HP':
     (embed, component) = QueueManager.add_player(inter.guild_id, queue_name)
   else:
     (embed, component) = QueueManager.create_queue_resources(inter.guild_id, queue_name)
